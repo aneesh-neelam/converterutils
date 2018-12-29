84c2eae6783cf99a3bc0018d5aaf2bb2f979f123
Aneesh Neelam <neelam.aneesh@gmail.com>
2018-12-29 17:57:47 +0530

    Postgresql SSL requirement is false

--- converterutils/settings.py
+++ converterutils/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'converterutils.wsgi.application'
 
 if os.environ.get('DATABASE_URL'):
     DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=False),
     }
 else:
     DATABASES = {
